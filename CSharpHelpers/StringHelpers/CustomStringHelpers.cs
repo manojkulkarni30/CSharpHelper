ca9fd66ad5fb109b4dd6f2a9c46b647aec6a1009
manojkulkarni30 <manojkulkarni30@gmail.com>
2015-07-08 19:35:32 +0530

    Initial Checkin

---
+++ CSharpHelpers/StringHelpers/CustomStringHelpers.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CSharpHelpers.StringHelpers
+{
+    public static class CustomStringHelpers
+    {
+        /// <summary>
+        /// Remove space from the String
+        /// </summary>
+        /// <param name="entity">String</param>
+        /// <returns></returns>
+        public static string RemoveSpace(this System.String entity)
+        {
+            return ReplaceParticularCharacter(entity, " ", String.Empty);
+        }
+
+        /// <summary>
+        /// Remove last character from the String
+        /// </summary>
+        /// <param name="entity">String</param>
+        /// <returns></returns>
+        public static string RemoveLastCharacter(this System.String entity)
+        {
+            return String.IsNullOrWhiteSpace(entity) ? String.Empty : entity.Remove((entity.Length - 1), 1);
+        }
+
+
+        /// <summary>
+        /// Remove first character from String
+        /// </summary>
+        /// <param name="entity">String</param>
+        /// <returns></returns>
+        public static string RemoveFirstCharacter(this System.String entity)
+        {
+            return String.IsNullOrWhiteSpace(entity) ? String.Empty : entity.Remove(0, 1);
+        }
+
+
+        /// <summary>
+        /// Create a string from an array of string using StringBuilder class and specified separator
+        /// </summary>
+        /// <param name="entities">List of string</param>
+        /// <param name="separator">Separator</param>
+        /// <param name="trim"></param>
+        /// <returns></returns>
+        public static string ToString(this System.String[] entities, string separator, bool trim = false)
+        {
+            StringBuilder str = new StringBuilder();
+            foreach (var item in entities)
+            {
+                str.Append(trim ? (item.Trim() + separator) : (item + separator));
+            }
+            return str.ToString().RemoveLastCharacter();
+        }
+
+
+        /// <summary>
+        /// Replace old character with new character in a string
+        /// </summary>
+        /// <param name="entity">String</param>
+        /// <param name="oldCharacter">Old character</param>
+        /// <param name="newCharacter">New Character</param>
+        /// <returns></returns>
+        public static string ReplaceParticularCharacter(this System.String entity, string oldCharacter, string newCharacter)
+        {
+            return entity.Replace(oldCharacter, newCharacter);
+        }
+
+        /// <summary>
+        /// Create MD5 Hash of a String
+        /// </summary>
+        /// <param name="entity">String</param>
+        /// <returns></returns>
+        public static string CreateMd5Hash(this System.String entity)
+        {
+            var md5Hash = MD5.Create();
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(entity));
+
+            StringBuilder sBuilder = new StringBuilder();
+            foreach (byte t in data)
+            {
+                sBuilder.Append(t.ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Convert the string to SEO friendly slug
+        /// </summary>
+        /// <param name="entity">String</param>
+        /// <returns></returns>
+        public static string ToSeoSlug(this System.String entity)
+        {
+            var str = entity.ReplaceParticularCharacter("&", "and");
+            // Remove non word characters
+            Regex rgx = new Regex("\\W");
+
+            // Remove multiple dash characters from the string
+            Regex rgx1 = new Regex("[-]+");
+
+            var removeMultipleDash = rgx1.Replace(rgx.Replace(str, "-"), "-");
+
+            var removeFirstDash = removeMultipleDash.StartsWith("-") ? removeMultipleDash.RemoveFirstCharacter() : removeMultipleDash;
+            var removeLastDash = removeFirstDash.EndsWith("-") ? removeFirstDash.RemoveLastCharacter() : removeFirstDash;
+            return removeLastDash.ToLower();
+        }
+    }
+}
---
+++ CSharpHelpers/readme.txt
@@ -0,0 +1,14 @@
+This package contains following String Helpers
+1. RemoveSpace : Remove space from string.
+
+2. RemoveLastCharacter : Remove last character from string.
+
+3. RemoveFirstCharacter : Remove first character from string.
+
+4. ToString : Create a string from an array of string using StringBuilder class and specified separator.
+
+5. ReplaceParticularCharacter : Replace the old character with new character in string.
+
+6. CreateMd5Hash : Create MD5 hash of a string.
+
+7. ToSeoSlug : Convert the string into SEO friendly slug
