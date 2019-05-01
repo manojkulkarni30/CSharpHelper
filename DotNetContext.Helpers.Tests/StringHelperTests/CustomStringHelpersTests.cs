2f28b259b6cb630645d0a8f5485500e047201b64
Manoj Kulkarni <manoj.kulkarni.3011@gmail.com>
2019-05-01 17:50:14 +0530

    Added More Test Cases

--- DotNetContext.Helpers.Tests/StringHelperTests/CustomStringHelpersTests.cs
+++ DotNetContext.Helpers.Tests/StringHelperTests/CustomStringHelpersTests.cs
@@ -25,5 +25,48 @@ namespace DotNetContext.Helpers.Tests.StringHelperTests
 
             Assert.AreEqual("WebDevelopment", result);
         }
+
+        [TestCase]
+        public void LastCharacterShouldBeRemovedWhenStringIsPass()
+        {
+            var testValue = "Web Development";
+
+            var result = testValue.RemoveLastCharacter();
+
+            Assert.AreEqual("Web Developmen", result);
+        }
+
+        [TestCase]
+        public void FirstCharacterShouldBeRemovedWhenStringIsPass()
+        {
+            var testValue = "Web Development";
+
+            var result = testValue.RemoveFirstCharacter();
+
+            Assert.AreEqual("eb Development", result);
+        }
+
+
+        [TestCase]
+        public void ResultShouldBeStringWhenStringArrayIsPassedWithSeperator()
+        {
+            var testValue = new string[] { "Web Development      ", "Azure Functions", "App Service" };
+
+            var result = testValue.ToString(",", trim: true);
+
+            var expectedResult = "Web Development,Azure Functions,App Service";
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestCase]
+        public void IsMD5HashOfStringIsCorrect()
+        {
+            var testValue = "Web Development";
+
+            var result = testValue.CreateMd5Hash();
+
+            var expectedResult = "e9f65fa4000d038efa0cdff8bb824632";
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }
